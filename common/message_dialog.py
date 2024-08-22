c3cd2cc221651eba72db3fd698588c9c0cddef0f
kpolok <kamil.polok99@gmail.com>
2024-08-22 13:59:07 +0200

    Decouple application logic from GUI messaging
    Introduce MessageHandler that takes care of
    creating message boxes, by which decouples
    the application logic from a speciffic GUI

---
+++ common/message_dialog.py
@@ -0,0 +1,100 @@
+from PySide2.QtWidgets import QDialog, QVBoxLayout, QApplication, QLabel, QPushButton, QHBoxLayout, QStyle, QSpacerItem, QSizePolicy
+from PySide2.QtGui import QFontMetrics
+from PySide2.QtCore import QSize, Qt
+
+class MessageDialog(QDialog):
+    '''
+    Custom QMessageBox designed to enhance window resizing flexibility and accommodate long titles. 
+    This custom dialog is particularly useful because it prevents the truncation of lengthy titles, 
+    ensuring they are fully displayed.
+    ''' 
+    def __init__(self, parent, title, message, iconType):
+        super().__init__(parent)
+
+        self.title = title
+        self.message = message
+        self.iconType = iconType
+
+        # Set window flags to keep only the title bar
+        self.setWindowFlags(Qt.WindowType.Dialog | Qt.WindowType.WindowTitleHint)
+
+        self.setWindowTitle(title)
+
+        self.layout = QVBoxLayout(self)
+
+        self._initMessage()
+        self._initButtons()
+
+        titleFont = QApplication.font()
+        fontMetrics = QFontMetrics(titleFont)
+        titleWidth = fontMetrics.horizontalAdvance(self.title) + 150  # Add some padding
+        messageWidth = 200 + 32 + 20  # Text width + icon width + padding
+        width = max(titleWidth, messageWidth)
+        self.setMinimumWidth(width)
+
+    def _initMessage(self):
+        # Message layout
+        messageLayout = QHBoxLayout()
+        self.layout.addLayout(messageLayout)
+
+        # Message type icon
+        self.iconLabel = QLabel()
+        self.iconLabel.setFixedWidth(50)
+        self.iconLabel.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        icon = self.style().standardIcon(self.iconType)
+        self.iconLabel.setPixmap(icon.pixmap(QSize(32, 32)))
+        messageLayout.addWidget(self.iconLabel)
+
+        # Message label 
+        self.messageLabel = QLabel(self.message)
+        self.messageLabel.setWordWrap(True)
+        messageLayout.addWidget(self.messageLabel)
+
+    def _initButtons(self):
+        # Buttons layout
+        buttonLayout = QHBoxLayout()
+        buttonLayout.setSpacing(20)
+        self.layout.addLayout(buttonLayout)
+
+        if self.iconType == QStyle.StandardPixmap.SP_MessageBoxQuestion:
+            buttonLayout.addItem(QSpacerItem(20, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum))
+
+            # Yes Button
+            self.yesButton = QPushButton("Tak")
+            self.yesButton.setFixedWidth(80)
+            self.yesButton.clicked.connect(self.accept)
+            buttonLayout.addWidget(self.yesButton)
+
+            # No button
+            self.noButton = QPushButton("Nie")
+            self.noButton.setFixedWidth(80)
+            self.noButton.clicked.connect(self.reject)
+            buttonLayout.addWidget(self.noButton)
+
+            buttonLayout.addItem(QSpacerItem(20, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum))
+        else:
+            # OK button
+            self.okButton = QPushButton("OK")
+            self.okButton.clicked.connect(self.accept)
+            self.okButton.setFixedWidth(80)
+            buttonLayout.addWidget(self.okButton)
+
+    @classmethod
+    def critical(cls, parent, title, message):
+        dialog = cls(parent, title, message, QStyle.StandardPixmap.SP_MessageBoxCritical)
+        dialog.exec_()
+
+    @classmethod
+    def information(cls, parent, title, message):
+        dialog = cls(parent, title, message, QStyle.StandardPixmap.SP_MessageBoxInformation)
+        dialog.exec_()
+
+    @classmethod
+    def question(cls, parent, title, message):
+        dialog = cls(parent, title, message, QStyle.StandardPixmap.SP_MessageBoxQuestion)
+        return dialog.exec_()
+
+    @classmethod
+    def warning(cls, parent, title, message):
+        dialog = cls(parent, title, message, QStyle.StandardPixmap.SP_MessageBoxWarning)
+        dialog.exec_()
---
+++ common/message_handler.py
@@ -0,0 +1,81 @@
+from PySide2.QtWidgets import QWidget
+
+from .message_dialog import MessageDialog
+
+from config import APP_NAME
+
+class MessageHandler:
+    """
+    Class for displying windows with information, critical
+    message or question. This class is not meant to be instantiated.
+    """
+    _base_title = APP_NAME
+    _icon = None
+
+    def __new__(cls, *args, **kwargs):
+        raise TypeError("This class cannot be instantiated")
+
+    @classmethod
+    def _title(cls, subtitle: str) -> str:
+        """
+        Concatenate the bas title with the subtitle
+
+        Args:
+            subtitle (str): additional window description
+        """
+        if subtitle:
+            return f"{cls._base_title} - {subtitle}"
+        else:
+            return cls._base_title
+
+    @classmethod
+    def critical(cls, parent: QWidget, title: str, message: str):
+        """
+        Show critical message.
+
+        Args:
+            parent (QWidget): Parent (UI) of the message box.
+            title (str): Description added to window title.
+            message (str): Message to display.
+        """
+        MessageDialog.critical(parent, cls._title(title), message)
+
+    @classmethod
+    def warning(cls, parent: QWidget, title: str, message: str):
+        """
+        Show warning message.
+
+        Args:
+            parent (QWidget): Parent (UI) of the message box.
+            title (str): Description added to window title.
+            message (str): Message to display.
+        """
+        MessageDialog.warning(parent, cls._title(title), message)
+
+    @classmethod
+    def information(cls, parent: QWidget, title: str, message: str):
+        """
+        Show information message.
+
+        Args:
+            parent (QWidget): Parent (UI) of the message box.
+            title (str): Description added to window title.
+            message (str): Message to display.
+        """
+        MessageDialog.information(parent, cls._title(title), message)
+
+    @classmethod
+    def question(cls, parent: QWidget, title: str, message: str) -> bool:
+        """
+        Show question.
+
+        Args:
+            parent (QWidget): Parent (UI) of the message box.
+            title (str): Description added to window title.
+            message (str): Message to display.
+        Returns:
+            (bool): response of the user (tak = True, nie = False)
+        """
+        res = MessageDialog.question(parent, cls._title(title), message)
+
+        return res == MessageDialog.DialogCode.Accepted
