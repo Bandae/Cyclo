36bb812c218023aad9288c5fd349d3b911aa1bf3
kpolok <kamil.polok99@gmail.com>
2024-09-10 21:43:34 +0200

    Change way of accessing resources

--- cyclo/config.py
+++ cyclo/config.py
@@ -1,27 +1,32 @@
-'''
-Config file for setting up the root directory of the application.
-It MUST be placed in the root of the project repository.
-'''
-
 import os
 import sys
 
-def dependencies_path(relative_path):
+# -----------------------------------BASE DIR PATH CONFIGURATION------------------------------------
+def get_base_dir_path():
+    """
+    Get the base directory path from which the dependencies will be looked up to.
+    Handles both development and bundled environments.
+    """
     # Check if running as a bundled application
     if getattr(sys, 'frozen', False):
         # If bundled, the executable's directory is the base path
-        base_path = os.path.dirname(sys.executable)
+        return os.path.dirname(sys.executable)
     else:
         # If running in a development environment, get 
-        # path to root directory of repository
-        base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        # path to root directory of repository - one directory level up
+        return os.path.normpath(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-    dependency_path = os.path.join(base_path, relative_path)
+BASE_DIR_PATH = get_base_dir_path()
+
+# ------------------------SET FUNCTION ACCESSING THE PROPPER DEPENDENCY PATH------------------------
+def dependencies_path(relative_path):
+    """
+    Resolves a relative path to an absolute path within the base directory.
+    """
+    dependency_path = os.path.join(BASE_DIR_PATH, os.path.normpath(relative_path))
     return dependency_path
 
-def resource_path(relative_path):
-    return os.path.normpath(os.path.join(relative_path))
-
+# --------------------------------------------CONSTANTS---------------------------------------------
 # Set application name and icon
 APP_NAME = 'Przekładnia Cykloidalna'
 APP_ICON = dependencies_path('resources//icons//mainwindow_icon.png')
